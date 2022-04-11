41b3e5a9ec7d68724d7eb1b5963e62ae90d59c85
Beck <guille_12.12@hotmail.com>
2022-04-11 19:25:26 -0300

    Juego Black Jack Finalizado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -10,7 +10,10 @@
 
 let deck = [];
 const tipos =  [ 'C','D','H','S' ];
-const especiales =  [ 'C','D','H','S' ];
+const especiales =  [ 'A','J','Q','H' ];
+
+
+// Crear un deck
 
 const crearDeck = () => {
 
@@ -18,14 +21,73 @@ const crearDeck = () => {
         for (let tipo of tipos) {
             deck.push(i + tipo)
         }
-
-
-
     } 
-    console.log( deck );
+
+    for( let tipo of tipos){
+        for (const especial of especiales) {
+            deck.push( especial + tipo)            
+        }
+    }
+
+    // console.log( deck );
+
+    deck = _.shuffle( deck );
+
+    // console.log( deck );
+
 }
 
 crearDeck();
+
+
+const pedirCarta = ()=> {
+
+    if ( deck.length === 0) {
+        throw 'Sin cartas'
+    }
+
+    const carta = deck.pop(); 
+    return carta;
+
+}
+
+// for( let i = 0; i <= 100; i++ ){
+//     pedirCarta();
+// }
+
+const valorCarta = ( carta ) => {
+ 
+    const valor = carta.substring( 0, carta.length -1 );
+    
+    return( isNaN( valor )) ? 
+            ( valor === A)? 11 : 10
+            : valor * 1;
+    
+    
+    
+    // console.log({valor});
+    // if( isNaN( valor )){
+
+    //     puntos = ( valor === A) ? 11 : 10;
+
+    //     // console.log('n');
+    // }else{
+    //     // console.log('!n');
+    //     puntos = valor * 1;
+    // }
+
+
+};
+
+const valor = valorCarta( pedirCarta() );
+// console.log({ valor });
+
+
+
+
+
+
+
 
 
 
